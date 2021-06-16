ca8fb3ec2cf992236ebccfec5a86cc4dff7a329b
xBlazeTECH <xBlazeTECH@users.noreply.github.com>
2021-06-16 12:39:14 -0400

    Update Syncing

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,28 +1,47 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  @Input()
-  awesomeThings: any[] = [];
-  newThing: any[] = [];
+export class HomeComponent implements OnInit, OnDestroy {
+  public awesomeThings: any[] = [];
+  public newThing: any = {};
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private socket: Socket) {}
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
+    this.fetchThings();
+  }
+
+  fetchThings(): void {
     this.http.get('/api/things').subscribe((awesomeThings: any) => {
       this.awesomeThings = awesomeThings;
     });
   }
 
   addThing(): void {
-    if (this.newThing === undefined) return;
+    if (!this.newThing.name || this.newThing.name === '') return;
+    this.http.post('/api/things', this.newThing).subscribe((err: any) => {
+      if (err) console.log(err);
+      this.fetchThings();
+      this.newThing = {};
+    });
   }
 
-  deleteThing(thing: any): void {}
+  deleteThing(thing: any): void {
+    this.http.delete(`/api/things/${thing._id}`).subscribe((err: any) => {
+      if (err) {
+        console.error(err);
+      }
+      this.fetchThings();
+    });
+  }
 }
